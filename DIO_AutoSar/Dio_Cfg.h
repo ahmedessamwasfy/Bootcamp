810290151710e1a82b23c750b2500da854c706ab
Muhamedhagag <47712323+Muhamedhagag@users.noreply.github.com>
2019-03-27 10:28:02 +0200

    delect

--- DIO_AutoSar/Dio_Cfg.h
+++
@@ -1,8 +0,0 @@
-#ifndef DIO_CFG_H_
-#define DIO_CFG_H_
-
-/* Pre-compile option for Development Error Detect */
-#define DIO_DEV_ERROR_DETECT        (STD_ON)
-#define MAX_SIZE 8
-
-#endif /* DIO_CFG_H_ */
--- DIO_AutoSar/SchM_Dio.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef SCHM_DIO_H_
-#define SCHM_DIO_H_
-
-
-
-
-
-#endif /* SCHM_DIO_H_ */
