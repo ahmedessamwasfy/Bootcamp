c2ac5c7db469083b730bf7af18f80b068171b413
Muhamedhagag <47712323+Muhamedhagag@users.noreply.github.com>
2019-04-21 17:25:44 +0200

    DELET

--- COM_TRANSMIT_ABDELHAY_TASK_ROUTER/allignment.h
+++
@@ -1,9 +0,0 @@
-
-
-#ifndef _ALL_H
-#define _ALL_H
-extern void Init_Task(void);
-extern void Allignment_Task(void);
-extern void Button_Task(void);
-
-#endif
--- COM_TRANSMIT_ABDELHAY_TASK_ROUTER/commen.h
+++
@@ -1,9 +0,0 @@
-
-
-#ifndef  COMMEN_H
-#define COMMEN_H
- #include "Types.h"
- #include "BitwiseOperations.h"
- #include "numbers.h"
- 
-#endif
